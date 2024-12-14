b79b0ad40ec072e4a2afc59dba4ffe2bd6b06790
mcmoodoo <hi@unpin.co>
2024-12-14 20:34:52 +0700

    font changed in both terminals

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+config.font = wezterm.font "HackNerdFont"
 config.color_scheme = 'Dracula'
 config.enable_wayland = true
 config.window_background_opacity = 0.85
